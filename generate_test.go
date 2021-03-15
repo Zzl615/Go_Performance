385f694136332d65dab402244295c2141f194d10
Noaghzil@615 <2966785870@qq.com>
2021-03-15 22:10:58 +0800

    Feat: 关于内存和耗时的基准测试

---
+++ generate_test.go
@@ -0,0 +1,50 @@
+// 生成一组长度为 n 的随机序列
+package main
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func generateWithCap(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0, n)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func generate(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+
+func BenchmarkGenerateWithCap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generateWithCap(1000000)
+	}
+}
+
+func BenchmarkGenerate(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generate(1000000)
+	}
+}
+
+// 开头小写，go test -bench 不执行
+func benchmarkGenerate(i int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generate(i)
+	}
+}
+
+func BenchmarkGenerate1000(b *testing.B)    { benchmarkGenerate(1000, b) }
+func BenchmarkGenerate10000(b *testing.B)   { benchmarkGenerate(10000, b) }
+func BenchmarkGenerate100000(b *testing.B)  { benchmarkGenerate(100000, b) }
+func BenchmarkGenerate1000000(b *testing.B) { benchmarkGenerate(1000000, b) }
